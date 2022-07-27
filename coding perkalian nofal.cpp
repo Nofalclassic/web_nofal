c37e40db940ff81d635eb6dc4affd16312b91688
Nofalclassic <110109447+Nofalclassic@users.noreply.github.com>
2022-07-27 20:46:01 +0700

    Add files via upload

---
+++ coding perkalian nofal.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    cout <<"MOH NOFALUL UMAM\n";
+    cout <<"\t\CODING PERKALIAN SEDERHANA\n";
+    cout <<"\t\t\ 4 VARIABLE\n";
+    cout <<endl;
+    cout <<"\t\t\PERKALIAN\n";
+    cout <<endl;
+    cout <<"Input Angka pertama :";cin>>a;
+    cout <<"Input Angka kedua :";cin>>b;
+    cout <<"Input Angka ketiga :";cin>>c;
+    cout <<"Input Angka keempat :";cin>>d;
+    cout <<"--------------------------------------\n";
+    
+    cout <<"Hasilnya adalah :"<<(a*b)<<endl;
+    cout <<endl;
+    cout <<"Hasilnya adalah :"<<(c*d)<<endl;   
+    cout <<endl;
+    cout <<"\t\t\SELALU ADA WAKTU UNTUK ORANG YANG BERUSAHA\n";
+}
